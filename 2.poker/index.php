cfca94766c5d9797516fac391483b63884d0cb93
Dmitry Voskoboynikov <dmitry.voskoboynikoff@gmail.com>
2021-07-05 10:30:39 +0300

    develop of poker

--- 2.poker/index.php
+++ 2.poker/index.php
@@ -3,3 +3,62 @@
 require_once("autoloader.php");
 
 
+$input = array(1, 1, 1, 1, 1);
+$res = array();
+
+foreach ($input as $value)
+{
+    $res[$value] = $res[$value] + 1;
+}
+
+print_r($res);
+
+// all numbers is different, then size of $res == 5
+if (sizeof($res) == 5)
+{
+    echo "all different";
+    return;
+}
+
+// size of $res is 4, one pair
+if (sizeof($res) == 4)
+{
+    echo "one pair";
+    return;
+}
+
+
+if (sizeof($res) == 3)
+{
+    foreach ($res as $key => $value)
+    {
+        if ($value == 3) {
+            echo "three of kind";
+            return;
+        }
+    }
+
+    echo "two pair";
+    return;
+}
+
+/**
+if (sizeof($res) == 2)
+{
+    foreach ($res as $key => $value)
+    {
+        if ($value == 4) {
+            echo "four of kind";
+            return;
+        }
+    }
+
+    echo ""
+}
+*/
+
+if (sizeof($res) == 1)
+{
+    echo "poker";
+    return;
+}
