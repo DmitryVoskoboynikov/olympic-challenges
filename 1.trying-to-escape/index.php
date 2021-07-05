f0207a216c633caa5612c5decba35254becd6f94
Dmitry Voskoboynikov <dmitry.voskoboynikoff@gmail.com>
2021-07-05 09:48:34 +0300

    poriton of code

--- 1.trying-to-escape/index.php
+++ 1.trying-to-escape/index.php
@@ -4,9 +4,9 @@ require_once("autoloader.php");
 
 //входная карта комнат
 $map = array();
-$map[] = array(1, 1, 1, 0, 1);
+$map[] = array(1, 1, 1, 1, 1);
 $map[] = array(1, 0, 1, 0, 1);
-$map[] = array(1, 0, 1, 1, 1);
+$map[] = array(1, 1, 1, 1, 1);
 
 //first room
 $startRoom = new \Machine\Room(2, 0);
--- 1.trying-to-escape/machine/processor.php
+++ 1.trying-to-escape/machine/processor.php
@@ -18,7 +18,7 @@ namespace Machine
         public function step($room)
         {
             // мы пришли в комнату в правом верхнем углу
-            if ($room->x == 0 && $room->y == 2)
+            if ($room->x == 0 && $room->y == 4)
             {
                 Registry::increaseNumberOfWays();
 
@@ -29,18 +29,45 @@ namespace Machine
             $y = $room->y;
             // здесь мы пытаемся пройти во все стороны из текущей комнаты
             // вниз
-            if (($x + 1) <= 2 && $this->map[$x + 1][$y]) {
+            if (($x + 1) <= 2 && $this->map[$x + 1][$y] && !$this->hasInWay($x + 1, $y, $room->p_room)) {
                 $newRoom = new Room($x + 1, $y, $room);
 
                 $this->step($newRoom);
             }
+
             //влево
-            if (($y - 1) >= 0 && $this->map[$x][$y - 1])
+            if (($y - 1) >= 0 && $this->map[$x][$y - 1] && !$this->hasInWay($x, $y - 1, $room->p_room))
             {
                 $newRoom = new Room($x, $y - 1, $room);
                 $this->step($newRoom);
             }
 
+            //вверх
+            if (($x - 1) >= 0 && $this->map[$x - 1][$y] && !$this->hasInWay($x - 1, $y, $room->p_room)) {
+                $newRoom = new Room($x - 1, $y, $room);
+
+                $this->step($newRoom);
+            }
+
+            //вправо
+            if (($y + 1 <= 4) && $this->map[$x][$y + 1] && !$this->hasInWay($x, $y + 1, $room->p_room)) {
+                $newRoom = new Room($x, $y + 1, $room);
+
+                $this->step($newRoom);
+            }
+
+        }
+
+        protected function hasInWay($x, $y, $room = null)
+        {
+            if (isset($room)) {
+                if ($x == $room->x && $y == $room->y)
+                    return true;
+                else
+                    return $this->hasInWay($x, $y, $room->p_room);
+            }
+
+            return false;
         }
 
     }
